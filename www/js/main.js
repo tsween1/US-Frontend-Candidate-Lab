3890b078c952d25543915d11ed784b5e195dced1
tims <tsween1@gmail.com>
2014-05-12 17:44:02 -0700

    changes

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,44 @@
 
+// JAVASCRIPT FORM VALIDATION
+
+function validateForm()
+{
+var x=document.forms["subscribe"]["fname"].value;
+if (x==null || x=="")
+  {
+  alert("Please Enter Email Address");
+  return false;
+  }
+}
+
+$(function(){
+	
+// JQUERY ANIMATION FOR INVITATION WINDOW
+
+	$(".invitation").animate({
+	    position: "absolute",
+		bottom: 0,
+	  }, 500, function() {
+	    // Animation complete.
+	  });
+
+// JQUERY CLICK EVENTS FOR RSVP BUTTONS
+
+	$( ".nav-yes").bind("click", function (event) {
+		$(".rsvp-yes").slideToggle("fast");
+	});
+	
+	$( ".nav-no").bind("click", function (event) {
+		$(".rsvp-no").slideToggle("fast");
+	});
+	
+	$( "#close-button-yes").bind("click", function (event) {
+		$(".rsvp-yes").hide();
+	});
+	
+	$( "#close-button-no").bind("click", function (event) {
+		$(".rsvp-no").hide();
+	});
+
+
+});
--- www/js/plugins.js
+++ www/js/plugins.js
@@ -22,3 +22,11 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+//SCRIPT FOR FACEBOOK BUTTON	
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
